37f77fd3a58583fef5f5a1c39df29f2be54b164a
Camila <camil.osorior@duocuc.cl>
2026-05-20 18:23:53 -0400

    test: configure all services to use H2 in-memory DB for tests

--- health-service/src/test/java/com/adoptapp/healthservice/HealthServiceApplicationTests.java
+++ health-service/src/test/java/com/adoptapp/healthservice/HealthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.adoptapp.healthservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class HealthServiceApplicationTests {
 
     @Test
